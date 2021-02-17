96b4ef3ff79c7816bda234be3ac91ee085338067
Thomas M <thomas5max@gmail.com>
2021-02-17 18:57:32 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="palkit",
-    version="0.2.0.dev1",
+    version="0.1.0",
     author="PAL",
     author_email="info@predictive-analytics-lab.com",
     description="Usefule functions",
