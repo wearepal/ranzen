1529baad5f9e6f4155a3596e9f6038203fec914b
thomkeh <thomas5max@gmail.com>
2021-03-17 23:41:41 +0000

    Update torch_utils_test.py

--- tests/torch_utils_test.py
+++ tests/torch_utils_test.py
@@ -5,7 +5,7 @@ import pytest
 from kit.torch.utils import Event
 
 
-def test_event():
+def test_event() -> None:
     with Event() as event:
         time.sleep(0.10)
     pytest.approx(event.time, 0.10)
