3142513714ec9fea56cb075000922259cfd0c14b
thomkeh <thomas5max@gmail.com>
2021-03-24 22:03:00 +0000

    Update kit/misc.py

--- kit/misc.py
+++ kit/misc.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from typing import Any, MutableMapping
 
 __all__ = ["flatten_dict"]
-
 def flatten_dict(
     d: MutableMapping[str, Any], parent_key: str = "", sep: str = "."
 ) -> dict[str, Any]:
