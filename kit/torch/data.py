590c3b2e0cb621a8765c07c6f58d58d1cab7047f
Myles Bartlett <44702700+MylesBartlett@users.noreply.github.com>
2021-06-10 18:04:50 +0100

    Infinite Sequential Sampler (#24)
    * Add an infinite, self-regenerating version of SequentialSampler.
    
    * Add code example and rename sampler to reflect that it produces batches of indexes.
    
    Co-authored-by: Myles Bartlett <m.bartlett@sussex.ac.uk>
    Co-authored-by: thomkeh <thomas5max@gmail.com>

--- kit/torch/data.py
+++ kit/torch/data.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
-from typing import Sequence
+from typing import Iterator, List, Sequence, Sized
 
 import numpy as np
 import torch
-from torch.utils.data import Dataset
+from torch import Tensor
+from torch.utils.data import Dataset, Sampler
 from torch.utils.data.dataset import Subset, random_split
 
-__all__ = ["prop_random_split"]
+from kit import implements
+
+__all__ = ["prop_random_split", "InfSequentialBatchSampler"]
 
 
 def prop_random_split(
@@ -26,3 +29,62 @@ def prop_random_split(
         section_sizes.append(len_ - sum(section_sizes))
     generator = torch.default_generator if seed is None else torch.Generator().manual_seed(seed)
     return random_split(dataset, section_sizes, generator=generator)
+
+
+class InfSequentialBatchSampler(Sampler[List[int]]):
+    r"""Infinitely samples elements sequentially, always in the same order.
+    This is useful for enabling iteration-based training.
+    Note that unlike torch's SequentialSampler which is an ordinary sampler that yields independent sample indexes,
+    this is a BatchSampler, requiring slightly different treatment when used with a DataLoader.
+
+    Example:
+        >>> batch_sampler = InfSequentialBatchSampler(data_source=train_data, batch_size=100, shuffle=True)
+        >>> train_loader = DataLoader(train_data, batch_sampler=batch_sampler, shuffle=False, drop_last=False) # drop_last and shuffle need to be False
+        >>> train_loader_iter = iter(train_loader)
+        >>> for _ in range(train_iters):
+        >>>     batch = next(train_loader_iter)
+
+    Args:
+        data_source (Sized): dataset to sample from
+    """
+
+    def __init__(self, data_source: Sized, batch_size: int, shuffle: bool = True) -> None:
+        self.data_source = data_source
+        self.shuffle = shuffle
+        self._dataset_size = len(data_source)
+        self.batch_size = min(batch_size, self._dataset_size)
+
+    def _generate_idx_seq(self) -> Tensor:
+        """Generate a random sequence of unique indexes."""
+        if self.shuffle:
+            return torch.arange(self._dataset_size)
+        return torch.randperm(self._dataset_size)
+
+    def batch_indexes(self, indexes: Tensor) -> Sequence[Tensor]:
+        """Split the indexes into batches."""
+        return indexes.split(self.batch_size)
+
+    @implements(Sampler)
+    def __iter__(self) -> Iterator[List[int]]:
+        batched_idxs_iter = iter(self.batch_indexes(self._generate_idx_seq()))
+        # Iterate until some externally-defined stopping criterion is reached
+        while True:
+            batch_idxs = next(batched_idxs_iter, None)  # type: ignore
+            if batch_idxs is None or (len(batch_idxs) < self.batch_size):
+                new_idx_seq = self._generate_idx_seq()
+                if batch_idxs is not None:
+                    # Rather than dropping the last batch if it is incomplete or simply using it,
+                    # incomplete as it may be, we take the alternative approach of concatenating the surplus
+                    # batch to the beginning of the next generation of indexes
+                    new_idx_seq = torch.cat([batch_idxs, new_idx_seq])
+                batched_idxs_iter = iter(self.batch_indexes(new_idx_seq))
+            else:
+                yield batch_idxs.tolist()
+
+    def __len__(self) -> None:
+        """The number of samples drawn.
+        Since the sampler is by design non-terminating, its length is undefined.
+        However__len__ still needs to be defined for downstream compatibility
+        (e.g. with PyTorch Lightning) and for this it suffices to simply return None.
+        """
+        return None
