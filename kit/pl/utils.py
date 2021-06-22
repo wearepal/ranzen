3aa4624ae8a24fb9f59306e8d38741e9d0960bc5
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-06-22 14:41:01 +0100

    Add iteration-based progress bar.

---
+++ kit/pl/utils.py
@@ -0,0 +1,99 @@
+import sys
+from typing import Any, Sequence
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.progress import ProgressBar, convert_inf, reset
+from torch import Tensor
+from tqdm import tqdm
+
+__all__ = ["IterationBasedProgBar"]
+
+
+class IterationBasedProgBar(ProgressBar):
+    def init_train_tqdm(self) -> tqdm:
+        """Override this to customize the tqdm bar for training."""
+        return tqdm(
+            desc="Training",
+            initial=self.train_batch_idx,
+            position=(2 * self.process_position),
+            disable=self.is_disabled,
+            leave=True,
+            dynamic_ncols=True,
+            file=sys.stdout,
+            smoothing=0,
+        )
+
+    def on_train_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
+        self._train_batch_idx = trainer.batch_idx
+        self.main_progress_bar = self.init_train_tqdm()
+        reset(self.main_progress_bar, trainer.max_steps)
+
+    def on_train_epoch_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
+        ...
+
+    def on_train_batch_end(
+        self,
+        trainer: pl.Trainer,
+        pl_module: pl.LightningModule,
+        outputs: list[Any],
+        batch: Sequence[Tensor],
+        batch_idx: int,
+        dataloader_idx: int,
+    ) -> None:
+        self._train_batch_idx += 1
+        if self._should_update(self.train_batch_idx, convert_inf(trainer.max_steps)):
+            self._update_bar(self.main_progress_bar)
+            self.main_progress_bar.set_postfix(trainer.progress_bar_dict)
+
+    def init_validation_tqdm(self) -> tqdm:
+        """Override this to customize the tqdm bar for validation."""
+        # The main progress bar doesn't exist in `trainer.validate()`
+        return tqdm(
+            desc="Validating",
+            disable=self.is_disabled,
+            leave=True,
+            dynamic_ncols=True,
+            file=sys.stdout,
+        )
+
+    def on_validation_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
+        self._val_batch_idx = 0
+        if trainer.sanity_checking:
+            reset(self.val_progress_bar, sum(trainer.num_sanity_val_batches))
+        else:
+            self.val_progress_bar = self.init_validation_tqdm()
+            self._update_bar(self.val_progress_bar)  # fill up remaining
+            reset(self.val_progress_bar, int(sum(trainer.num_val_batches)))
+
+    def on_validation_batch_end(
+        self,
+        trainer: pl.Trainer,
+        pl_module: pl.LightningModule,
+        outputs: list[Any],
+        batch: Sequence[Tensor],
+        batch_idx: int,
+        dataloader_idx: int,
+    ) -> None:
+        self._update_bar(self.val_progress_bar)
+
+    def on_validation_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
+        self.val_progress_bar.close()
+
+    def on_test_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
+        self._test_batch_idx = 0
+        self.test_progress_bar = self.init_test_tqdm()
+        reset(self.test_progress_bar)
+        self.test_progress_bar.total = convert_inf(sum(trainer.num_test_batches))
+
+    def on_test_batch_end(
+        self,
+        trainer: pl.Trainer,
+        pl_module: pl.LightningModule,
+        outputs: list[Any],
+        batch: Sequence[Tensor],
+        batch_idx: int,
+        dataloader_idx: int,
+    ) -> None:
+        self._test_batch_idx += 1
+        if self._should_update(self.test_batch_idx, sum(trainer.num_test_batches)):
+            self._update_bar(self.test_progress_bar)
