04d7abff7a0e1b68614c4cbdd85b0764facf140c
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-07-07 23:53:59 +0100

    Add return type to test function.

--- tests/torch_data_test.py
+++ tests/torch_data_test.py
@@ -13,7 +13,7 @@ def dummy_ds() -> TensorDataset:
 
 
 @pytest.mark.parametrize("props", [0.5, [-0.2, 0.5], [0.1, 0.3, 0.4], [0.5, 0.6]])
-def test_prop_random_split(dummy_ds: TensorDataset, props: float | list[float]):
+def test_prop_random_split(dummy_ds: TensorDataset, props: float | list[float]) -> None:
     sum_ = props if isinstance(props, float) else sum(props)
     props_ls = [props] if isinstance(props, float) else props
     if sum_ > 1 or any((not (0 <= prop <= 1)) for prop in props_ls):
