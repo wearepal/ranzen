4539defcb0538bdd1c9b4dfd14d45e66b885f9a1
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-08-14 21:47:00 +0100

    Simplify the ITS code slightly by removing redundant 'round'.

--- kit/torch/transforms/mixup.py
+++ kit/torch/transforms/mixup.py
@@ -137,17 +137,14 @@ class RandomMixUp:
             # with samples from other groups. This can be efficiently done as follows:
             # 1) Sample uniformly from the range [0, diff_group_count - 1] to obtain the
             # groupwise indices. This involves first sampling from the standard uniform distribution
-            # and then rescaling to [-diff_group_count, diff_group_count], followed by clamping, so that
-            # 0 and diff_group_count have the same probability of being drawn as any other value.
-            # The uniform samples are mapped to indices by multipling by diff_group_counts
-            # and rounding. randint is unsuitable here because the groups aren't guaranteed to
-            # have equal cardinality (using it to sample from the cyclic group, Z / diff_group_count Z,
-            # as above, leads to biased sampling).
+            # and then rescaling to [-diff_group_count, diff_group_count] so that 0 and diff_group_count
+            # have the same probability of being drawn as any other value.  The uniform samples are
+            # mapped to indices by multipling by diff_group_counts and rounding. randint is unsuitable
+            # here because the groups aren't guaranteed to have equal cardinality (using it to sample
+            # from the cyclic group, Z / diff_group_count Z, as above, leads to biased sampling).
             step_size = diff_group_counts.reciprocal()
-            u = (
-                (torch.rand(num_selected, device=inputs.device) * (1 + (2 * step_size))) - step_size
-            ).clamp(min=0, max=1)
-            rel_pair_indices = (u * (diff_group_counts - 1)).round().long()
+            u = (torch.rand(num_selected, device=inputs.device) * (1 + (2 * step_size))) - step_size
+            rel_pair_indices = (u.clamp(min=0, max=1) * (diff_group_counts - 1)).long()
             # 2) Convert the row-wise indices into row-major indices, considering only
             # only the postive entries in the rows.
             rel_pair_indices[1:] += diff_group_counts.cumsum(dim=0)[:-1]
