65fec89ddb9b4cea193ad824a236b6ba4c185cbe
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-09-21 11:23:28 +0100

    Reduce maxsize of lru_cache to 32.

--- kit/hydra/relay.py
+++ kit/hydra/relay.py
@@ -35,7 +35,7 @@ __all__ = [
 ]
 
 
-@lru_cache()
+@lru_cache(maxsize=32)
 def _camel_to_snake(name: str) -> str:
     name = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
     return re.sub("([a-z0-9])([A-Z])", r"\1_\2", name).lower()
