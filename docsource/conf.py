f9e0cd81d123bf465ca69c785887a9297e3b0bab
Thomas M <thomas5max@gmail.com>
2021-09-28 14:33:34 +0100

    Use read-the-docs theme for docs

--- docsource/conf.py
+++ docsource/conf.py
@@ -77,8 +77,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "nature"
-html_sidebars = {"**": ["globaltoc.html", "relations.html", "sourcelink.html", "searchbox.html"]}
+# html_theme = "nature"
+# html_sidebars = {"**": ["globaltoc.html", "relations.html", "sourcelink.html", "searchbox.html"]}
+html_theme = "sphinx_rtd_theme"  # 'alabaster'
+pygments_style = "sphinx"  # syntax highlighting style to use
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
