d7d82d90a7ad87741f7b8f604081e4ea7cc52eee
Myles Bartlett <m.bartlett@sussex.ac.uk>
2021-09-29 18:16:37 +0100

    palmantra -> mantra.

--- docsource/conf.py
+++ docsource/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
-project = "Palmantra"
+project = "mantra"
 copyright = "2021, PAL"
 author = "PAL"
 
