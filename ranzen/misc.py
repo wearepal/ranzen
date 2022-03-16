d24e9e9f2ad8dec6869908d3b34f529e67dc6db1
Myles Bartlett <m.bartlett@sussex.ac.uk>
2022-03-16 23:45:23 +0000

    Add properly-typed re-implementation of partial.

--- ranzen/misc.py
+++ ranzen/misc.py
@@ -1,9 +1,17 @@
 from __future__ import annotations
 import copy
 from enum import Enum
-from typing import Any, MutableMapping, TypeVar, overload
+from reprlib import recursive_repr
+from typing import Any, Callable, Generic, MutableMapping, TypeVar, overload
 
-__all__ = ["flatten_dict", "gcopy", "str_to_enum"]
+from typing_extensions import Self
+
+__all__ = [
+    "flatten_dict",
+    "gcopy",
+    "partial",
+    "str_to_enum",
+]
 
 
 def flatten_dict(
@@ -98,3 +106,85 @@ def str_to_enum(str_: str | E, *, enum: type[E]) -> E:
         raise TypeError(
             f"'{str_}' is not a valid option for enum '{enum.__name__}'; must be one of {valid_ls}."
         )
+
+
+R = TypeVar("R", covariant=True)
+
+
+class partial(Generic[R]):
+    """New function with partial application of the given arguments
+    and keywords.
+
+    This is a re-implementation of :class:`functools.partial` with proper type-support.
+    """
+
+    __slots__ = "func", "kwargs", "__dict__", "__weakref__"
+
+    def __new__(cls: type[Self], func: Callable[..., R], **kwargs: Any) -> Self:
+        """
+        :param func: Callable to undergo partial partial application.
+
+        :returns: ``func`` with ``kwargs`` partially applied.
+        """
+        if not callable(func):
+            raise TypeError("the first argument must be callable")
+
+        if isinstance(func, Self):
+            kwargs = {**func.kwargs, **kwargs}
+            func = func.func
+
+        self = super(partial, cls).__new__(cls)
+        self.func = func
+        self.kwargs = kwargs
+        return self
+
+    def __call__(self, *args: Any, **kwargs: Any) -> R:
+        kwargs = {**self.kwargs, **kwargs}
+        return self.func(*args, **kwargs)
+
+    @recursive_repr()
+    def __repr__(self) -> str:
+        qualname = type(self).__qualname__
+        args = [repr(self.func)]
+        args.extend(f"{k}={v!r}" for (k, v) in self.kwargs.items())
+        return f"{qualname}({', '.join(args)})"
+
+    def __reduce__(
+        self,
+    ) -> tuple[
+        type[Self],
+        tuple[Callable[..., R]],
+        tuple[Callable[..., R], Any | None, dict[str, Any] | None],
+    ]:
+        return (
+            type(self),
+            (self.func,),
+            (self.func, self.kwargs or None, self.__dict__ or None),
+        )
+
+    def __setstate__(
+        self,
+        state: tuple[Callable[..., R], Any | None, dict[str, Any] | None],
+    ) -> None:
+        if not isinstance(state, tuple):
+            raise TypeError("argument to __setstate__ must be a tuple")
+        if len(state) != 3:
+            raise TypeError(f"expected 3 items in state, got {len(state)}")
+        func, kwds, namespace = state
+        if (
+            not callable(func)
+            or (kwds is not None and not isinstance(kwds, dict))
+            or (namespace is not None and not isinstance(namespace, dict))
+        ):
+            raise TypeError("invalid partial state")
+
+        if kwds is None:
+            kwds = {}
+        elif type(kwds) is not dict:  # XXX does it need to be *exactly* dict?
+            kwds = dict(kwds)
+        if namespace is None:
+            namespace = {}
+
+        self.__dict__ = namespace
+        self.func = func
+        self.kwargs = kwds
