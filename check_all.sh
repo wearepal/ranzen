21c57d434e01eab8725c3088507f3f74b4fe9494
Myles Bartlett <m.bartlett@sussex.ac.uk>
2022-11-02 18:59:07 +0000

    Run black on ranzen/tests specifically in check_all.

--- check_all.sh
+++ check_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Begin check..." \
-&& black . \
+&& black ranzen \
+&& black tests \
 && python -m pytest -vv tests/ \
 && darglint --docstring-style sphinx -v 2 -z long ranzen \
 && pyright ranzen \
