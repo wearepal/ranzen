38cda9c9ced0451cb8b72b14712e1c9e7097c77b
Thomas M Kehrenberg <tmke8@posteo.net>
2023-09-26 13:48:04 +0200

    Fix mock import

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ autodoc_type_aliases = {
     "pl.LightningModule": "pytorch_lightning.LightningModule",
 }
 autodoc_mock_imports = [
-    "attr",
+    "attrs",
     "hydra",
     "loguru",
     "numpy",
