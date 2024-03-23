14acdfd2a9f314cec6ee748aef06d8f5b6af8885
Thomas M Kehrenberg <tmke8@posteo.net>
2024-03-23 17:53:01 +0100

    Improve wording slightly

--- ranzen/hydra/utils.py
+++ ranzen/hydra/utils.py
@@ -220,9 +220,10 @@ def register_hydra_config(
                         )
             elif entry.name in groups:
                 raise ValueError(
-                    f"Entry `{entry.name}` has registered variants, but its type, "
+                    f"Entry `{entry.name}` has registered variants, but its type annotation, "
                     f"`{getattr(typ, '__name__', str(typ))}`, is not a dataclass. "
                     "(Note that unions of dataclasses are not allowed either.)"
+                    "You can always use `Any` for the type annotation."
                 )
 
     cs = ConfigStore.instance()
